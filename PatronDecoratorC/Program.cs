01cd614725d1ee81cad83c3ae4aef28be819d54a
Ashley <Ashley@DESKTOP-G9K8E0I>
2022-10-17 19:42:43 -0500

    Agregar archivos de proyecto.

---
+++ PatronDecoratorC/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronDecoratorC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BebidaComponent cafe = new CafeDescafeinado();
+            cafe = new Crema(cafe);
+            cafe = new Edulcorante(cafe);
+            cafe = new Canela(cafe);
+
+            Console.WriteLine($"Producto:  {cafe.Descripcion} tiene un costo de : ${cafe.Costo}");
+
+            cafe = new CafeExpresso();
+            cafe = new Leche(cafe);
+            cafe = new Edulcorante(cafe);
+            cafe = new Canela(cafe);
+
+            Console.WriteLine($"Producto:  {cafe.Descripcion} tiene un costo de : ${cafe.Costo}");
+
+            cafe = new CafeSolo();
+            cafe = new Edulcorante(cafe);
+
+            Console.WriteLine($"Producto:  {cafe.Descripcion} tiene un costo de : ${cafe.Costo}");
+
+            BebidaComponent te = new TeTradicional();
+
+            te = new Azucar(te);
+
+            Console.WriteLine($"Producto:  {te.Descripcion} tiene un costo de : ${te.Costo}");
+
+            Console.ReadKey();
+        }
+    }
+}
